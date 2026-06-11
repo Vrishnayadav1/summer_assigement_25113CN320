f9dfa1509fdc54d176261288a8e629fa21a764b6
vrishna yadav <yadavvrishna@gmail.com>
2026-06-11 10:42:23 +0530

    Day-9 assignment is uploaded

---
+++ Day-9/repeatedcharacter.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("enter the number of row");
+    scanf("%d",&n);
+     int a = 1;
+
+        for(int i=1; i<=n; i++){
+            
+    
+
+        //A 
+        //B B 
+        //C C C 
+        for(int j=1; j<=i; j++){
+            
+            int d = a + 64;
+             char ch = (char)d;
+            printf("%c ",ch);      
+        }
+        a++;
+        printf("\n");
+    }
+    return 0;
+}
---
+++ Day-9/reversestar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++){  //outer loop ->no of lines
+        for(int j=1; j<=n-i+1; j++){ //inner loop -> no of stars in each line
+       
+            printf("* ");  ///1  2 3 4
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
